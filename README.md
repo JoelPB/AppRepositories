fbe4f2422488191ca8f581d381f68b111220a808
JoelPB <coffee.program.joel@gmail.com>
2021-07-13 09:10:41 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+- Projeto para o curso Criando um App para Apresentar seu Portfólio do GitHub da Digital Innovation One
+
---
+++ app/src/main/java/joelpb/com/github/apprepositories/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package joelpb.com.github.apprepositories.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import joelpb.com.github.apprepositories.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
